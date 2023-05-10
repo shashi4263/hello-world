ff919606e739a42c199a02d551b762e90f3cf95a
shashi4263 <72293955+shashi4263@users.noreply.github.com>
2023-05-10 23:51:09 +0530

    Update kth.cpp

--- algocode/kth.cpp
+++ algocode/kth.cpp
@@ -1,71 +1,27 @@
+// C++ program to find K'th smallest element
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition(vector<int> &v, int lb, int ub)
+// Function to return K'th smallest element in a given array
+int kthSmallest(int arr[], int N, int K)
 {
-    int key = v[ub]; // pivot element
-    int lo = lb - 1;
-    for (int i = lb; i < ub; i++)
-    {
-        if (v[i] <= key)
-        {
-            swap(v[i], v[++lo]);
-        }
-    }
-    swap(v[ub], v[++lo]);
-    return lo;
+    // Sort the given array
+    sort(arr, arr + N);
+
+    // Return k'th element in the sorted array
+    return arr[K - 1];
 }
 
-int kst(vector<int> &v, int q, int lb, int ub)
-{
-    while (lb <= ub)
-    {
-        int x = partition(v, lb, ub);
-        if (x == q)
-        {
-            return v[x];
-        }
-        else if (q < x)
-        {
-            return (kst(v, q, 0, x - 1));
-        }
-        else
-        {
-            return (kst(v, q, x + 1, ub));
-        }
-    }
-    return -1;
-}
-
+// Driver's code
 int main()
 {
-    vector<int> v;
-    int n;
-    cout << "enter the total number of element -> ";
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cout << "enter " << (i + 1) << " th element -> ";
-        int x;
-        cin >> x;
-        v.push_back(x);
-    }
+    int arr[] = {12, 3, 5, 7, 19};
+    int N = sizeof(arr) / sizeof(arr[0]), K = 2;
 
-    cout << "enter the position -> ";
-    int q;
-    cin >> q;
-    cout << kst(v, q - 1, 0, n - 1) << endl;
+    // Function call
+    cout << "K'th smallest element is "
+         << kthSmallest(arr, N, K);
     return 0;
 }
 
-// output
-/*
-enter the total number of element -> 5
-enter 1 th element -> 2
-enter 2 th element -> 3
-enter 3 th element -> 4
-enter 4 th element -> 6
-enter 5 th element -> 24
-enter the position -> 4
-6
-*/
+// K'th smallest element is 5
